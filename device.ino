b6c9a9fe3242107844e9c0891e70ad3b961fd630
Stefano Lucchesini <slucchesini@tws-electronics.it>
2021-11-22 14:12:56 +0100

    Primo commit

---
+++ device.ino
@@ -0,0 +1,152 @@
+/**
+ * A simple Azure IoT example for sending telemetry to Iot Hub.
+ */
+
+#include <WiFi.h>
+#include "Esp32MQTTClient.h"
+#include <WiFiManager.h> 
+
+#define INTERVAL 10000
+#define MESSAGE_MAX_LEN 256
+
+/*String containing Hostname, Device Id & Device Key in the format:                         */
+/*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
+/*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
+static const char* connectionString = "HostName=geniale-iothub.azure-devices.net;DeviceId=00000001;SharedAccessKey=Cn4UylzZVDZD8UGzCTJazR3A9lRLnB+CbK6NkHxCIMk=";
+const char *messageData = "{\"messageId\":%d, \"Temperature\":%f, \"Humidity\":%f}";
+
+static bool hasIoTHub = false;
+static bool hasWifi = false;
+int messageCount = 1;
+static bool messageSending = true;
+static uint64_t send_interval_ms;
+
+
+static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
+{
+  if (result == IOTHUB_CLIENT_CONFIRMATION_OK)
+  {
+    Serial.println("Send Confirmation Callback finished.");
+  }
+}
+
+static void MessageCallback(const char* payLoad, int size)
+{
+  Serial.println("Message callback:");
+  Serial.println(payLoad);
+}
+
+static void DeviceTwinCallback(DEVICE_TWIN_UPDATE_STATE updateState, const unsigned char *payLoad, int size)
+{
+  char *temp = (char *)malloc(size + 1);
+  if (temp == NULL)
+  {
+    return;
+  }
+  memcpy(temp, payLoad, size);
+  temp[size] = '\0';
+  // Display Twin message.
+  Serial.println(temp);
+  free(temp);
+}
+
+static int  DeviceMethodCallback(const char *methodName, const unsigned char *payload, int size, unsigned char **response, int *response_size)
+{
+  LogInfo("Try to invoke method %s", methodName);
+  const char *responseMessage = "\"Successfully invoke device method\"";
+  int result = 200;
+
+  if (strcmp(methodName, "start") == 0)
+  {
+    LogInfo("Start sending temperature and humidity data");
+    messageSending = true;
+  }
+  else if (strcmp(methodName, "stop") == 0)
+  {
+    LogInfo("Stop sending temperature and humidity data");
+    messageSending = false;
+  }
+  else
+  {
+    LogInfo("No method %s found", methodName);
+    responseMessage = "\"No method found\"";
+    result = 404;
+  }
+
+  *response_size = strlen(responseMessage) + 1;
+  *response = (unsigned char *)strdup(responseMessage);
+
+  return result;
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("ESP32 Device");
+  Serial.println("Initializing...");
+  Serial.println(" > WiFi");
+  Serial.println("Starting connecting WiFi.");
+
+  delay(10);
+
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // it is a good practice to make sure your code sets wifi mode how you want it.
+  WiFiManager wm;
+  //wm.resetSettings();  // reset settings - wipe stored credentials for testing
+  bool res;
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  res = wm.autoConnect("GENIALE"); // anonymous ap
+  //res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+  if(!res) {
+      Serial.println("Failed to connect to wifi");
+      // ESP.restart();
+  } 
+  else {
+      //if you get here you have connected to the WiFi    
+      Serial.println("Connected to wifi...)");
+      hasWifi = true;
+    }
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(" > IoT Hub");
+  if (!Esp32MQTTClient_Init((const uint8_t*)connectionString, true))
+  {
+    hasIoTHub = false;
+    Serial.println("Initializing IoT hub failed.");
+    return;
+  }
+  hasIoTHub = true;
+  Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
+  Esp32MQTTClient_SetMessageCallback(MessageCallback);
+  Esp32MQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
+  Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
+  Serial.println("Start sending events.");
+  randomSeed(analogRead(0));
+  send_interval_ms = millis();
+}
+
+void loop() {
+if (hasWifi && hasIoTHub)
+  {
+    if (messageSending && 
+        (int)(millis() - send_interval_ms) >= INTERVAL)
+    {
+      // Send teperature and humidity data
+      char messagePayload[MESSAGE_MAX_LEN];
+      float temperature = (float)random(0,500)/10;
+      float humidity = (float)random(0, 1000)/10;
+      snprintf(messagePayload, MESSAGE_MAX_LEN, messageData, messageCount++, temperature, humidity);
+      Serial.println(messagePayload);
+      EVENT_INSTANCE* message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
+      Esp32MQTTClient_SendEventInstance(message);
+      send_interval_ms = millis();
+    }
+    else
+    {
+      Esp32MQTTClient_Check();
+    }
+  }
+  delay(10);
+}
