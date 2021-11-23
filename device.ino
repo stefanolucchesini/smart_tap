4493b0e20dd1a5ad351eb3bd153f74ad93ad640b
Stefano Lucchesini <slucchesini@tws-electronics.it>
2021-11-23 09:11:19 +0100

    Aggiunto modulo contatore impulsi

--- device.ino
+++ device.ino
@@ -5,10 +5,23 @@
 #include <WiFi.h>
 #include "Esp32MQTTClient.h"
 #include <WiFiManager.h> 
+#include "driver/pcnt.h"   //Pulse counter library
 
-#define INTERVAL 10000
+
+#define INTERVAL 10000  // IoT message sending interval in ms
 #define MESSAGE_MAX_LEN 256
 
+//// PULSE COUNTER MODULE ////
+#define PCNT_FREQ_UNIT      PCNT_UNIT_0     // select ESP32 pulse counter unit 0 (out of 0 to 7 indipendent counting units)
+                                            // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/pcnt.html
+int16_t PulseCounter =     0;                                // pulse counter, max. value is 65536
+int OverflowCounter =      0;                                // pulse counter overflow counter
+int PCNT_H_LIM_VAL =       30000;                            // upper limit of counting  max. 32767, write +1 to overflow counter, when reached 
+uint16_t PCNT_FILTER_VAL=  0;                             // filter (damping, inertia) value for avoiding glitches in the count, max. 1023
+pcnt_isr_handle_t user_isr_handle = NULL;                 // user interrupt handler (not used)
+int liters = 0;
+#define PULSES_PER_LITER 165
+////  MICROSOFT AZURE IOT DEFINITIONS   ////
 /*String containing Hostname, Device Id & Device Key in the format:                         */
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
@@ -21,6 +34,8 @@ int messageCount = 1;
 static bool messageSending = true;
 static uint64_t send_interval_ms;
 
+////  I/Os definitions    ////
+#define PCNT_INPUT_SIG_IO   34       // Flow sensor connected to GPIO34
 
 static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
 {
@@ -79,9 +94,49 @@ static int  DeviceMethodCallback(const char *methodName, const unsigned char *pa
   return result;
 }
 
+//// PULSE COUNTER OVERFLOW ISR ////
+  void IRAM_ATTR CounterOverflow(void *arg) {                  // Interrupt for overflow of pulse counter
+    OverflowCounter = OverflowCounter + 1;                     // increase overflow counter
+    PCNT.int_clr.val = BIT(PCNT_FREQ_UNIT);                    // clean overflow flag
+    pcnt_counter_clear(PCNT_FREQ_UNIT);                        // zero and reset of pulse counter unit
+  }
 
+  void initPulseCounter (){                                    // initialise pulse counter
+    pcnt_config_t pcntFreqConfig = { };                        // Instance of pulse counter
+    pcntFreqConfig.pulse_gpio_num = PCNT_INPUT_SIG_IO;         // pin assignment for pulse counter
+    pcntFreqConfig.pos_mode = PCNT_COUNT_INC;                  // count rising edges (=change from low to high logical level) as pulses
+    pcntFreqConfig.neg_mode = PCNT_COUNT_DIS;                  // do nothing on falling edges
+    pcntFreqConfig.counter_h_lim = PCNT_H_LIM_VAL;             // set upper limit of counting 
+    pcntFreqConfig.unit = PCNT_FREQ_UNIT;                      // select ESP32 pulse counter unit 0
+    pcntFreqConfig.channel = PCNT_CHANNEL_0;                   // select channel 0 of pulse counter unit 0
+    pcnt_unit_config(&pcntFreqConfig);                         // configure rigisters of the pulse counter
+  
+    pcnt_counter_pause(PCNT_FREQ_UNIT);                        // pause pulse counter unit
+    pcnt_counter_clear(PCNT_FREQ_UNIT);                        // zero and reset of pulse counter unit
+  
+    pcnt_event_enable(PCNT_FREQ_UNIT, PCNT_EVT_H_LIM);         // enable event for interrupt on reaching upper limit of counting
+    pcnt_isr_register(CounterOverflow, NULL, 0, &user_isr_handle);  // configure register overflow interrupt handler
+    pcnt_intr_enable(PCNT_FREQ_UNIT);                          // enable overflow interrupt
+
+    pcnt_set_filter_value(PCNT_FREQ_UNIT, PCNT_FILTER_VAL);    // set damping, inertia 
+    pcnt_filter_enable(PCNT_FREQ_UNIT);                        // enable counter glitch filter (damping)
+  
+    pcnt_counter_resume(PCNT_FREQ_UNIT);                       // resume counting on pulse counter unit
+  }
+   
+  void Reset_PCNT() {                                          // function resetting counter 
+    OverflowCounter = 0;                                       // set overflow counter to zero
+    pcnt_counter_clear(PCNT_FREQ_UNIT);                        // zero and reset of pulse counter unit
+  }
+
+  void get_liters(){                                    // converts the pulses received from fl1 to liters
+      pcnt_get_counter_value(PCNT_FREQ_UNIT, &PulseCounter);     // get pulse counter value - maximum value is 16 bit
+      liters = ( OverflowCounter*PCNT_EVT_H_LIM + PulseCounter ) / PULSES_PER_LITER;
+  }
 
 void setup() {
+  pinMode(PCNT_INPUT_SIG_IO, INPUT);                  // the output of the flow sensor is open collector (MUST USE EXTERNAL PULL UP!!)
+  initPulseCounter();
   Serial.begin(115200);
   Serial.println("ESP32 Device");
   Serial.println("Initializing...");
@@ -142,6 +197,8 @@ if (hasWifi && hasIoTHub)
       EVENT_INSTANCE* message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
       Esp32MQTTClient_SendEventInstance(message);
       send_interval_ms = millis();
+      get_liters();
+      Serial.println("Liters: %d", liters);
     }
     else
     {
